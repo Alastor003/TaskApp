7f15e40dc4c9f5e80310c4ffc4b1fab2c74c2680
Brian Ferreira <bri.ferreira03@gmail.com>
2023-10-15 15:53:12 -0300

    se hace logica de login

--- app/src/main/java/com/example/taskapp/MainActivity.java
+++ app/src/main/java/com/example/taskapp/MainActivity.java
@@ -2,21 +2,34 @@ package com.example.taskapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
-import android.view.translation.ViewTranslationRequest;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
 
     //Referencias
-    Button btnRegistro;
+    EditText edtUser, edtPass;
+    Button btnRegistro, btnLogin;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        edtUser = findViewById(R.id.edtUser);
+        edtPass = findViewById(R.id.edtPass);
+        btnLogin = findViewById(R.id.btnLogin);
         btnRegistro = findViewById(R.id.registrar);
 
         btnRegistro.setOnClickListener(new View.OnClickListener() {
@@ -26,5 +39,41 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(registro);
             }
         });
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String usuario = edtUser.getText().toString();
+                String contrasenia = edtPass.getText().toString();
+
+                if (iniciarSesion(usuario, contrasenia)) {
+                    Toast.makeText(MainActivity.this, "Se inicio sesión con éxito.", Toast.LENGTH_SHORT).show();
+                    Intent intent = new Intent(MainActivity.this, Tarea.class);
+                    startActivity(intent);
+                } else {
+                    Toast.makeText(MainActivity.this, "Inicio de sesión fallido.", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
+
+    private boolean iniciarSesion(String usuario, String contrasenia) {
+
+        File file = new File(getFilesDir(), "usuarios.csv");
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader(file));
+            String line;
+            while ((line = reader.readLine()) != null) {
+                String[] userData = line.split(",");
+                if (userData.length >= 4 && usuario.equals(userData[1]) && contrasenia.equals(userData[3])) {
+                    return true;
+                }
+            }
+            reader.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
+
 }
--- app/src/main/java/com/example/taskapp/Registro.java
+++ app/src/main/java/com/example/taskapp/Registro.java
@@ -6,22 +6,91 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+import android.widget.ImageButton;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 
 public class Registro extends AppCompatActivity {
-
-    Button btnCancelar;
+    EditText edtNombre, edtUsuario, edtEmail, edtContrasenia;
+    Button btnRegistrar;
+    ImageButton btnBack;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registro);
 
-        btnCancelar = findViewById(R.id.btnCancelar);
+        edtNombre = findViewById(R.id.edtNombre);
+        edtUsuario = findViewById(R.id.edtUsuario);
+        edtEmail = findViewById(R.id.edtEmail);
+        edtContrasenia = findViewById(R.id.edtContrasenia);
+        btnRegistrar = findViewById(R.id.btnRegistrar);
+        btnBack = findViewById(R.id.btnBack);
 
-        btnCancelar.setOnClickListener(new View.OnClickListener() {
+
+        btnBack.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 finish();
             }
         });
+
+        btnRegistrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String nombre = edtNombre.getText().toString();
+                String usuario = edtUsuario.getText().toString();
+                String email = edtEmail.getText().toString();
+                String contrasenia = edtContrasenia.getText().toString();
+
+                if (usuarioExiste(usuario, email)) {
+                    Toast.makeText(Registro.this, "Usuario o correo electrónico ya existen", Toast.LENGTH_SHORT).show();
+                } else {
+                    guardarDatosUsuario(nombre, usuario, email, contrasenia);
+
+                    Intent intent = new Intent(Registro.this, MainActivity.class);
+                    startActivity(intent);
+                    Toast.makeText(Registro.this, "Registro completo", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+
+    private void guardarDatosUsuario(String nombre, String usuario, String email, String contrasenia) {
+        File file = new File(getFilesDir(), "usuarios.csv");
+        try {
+            BufferedWriter writer = new BufferedWriter(new FileWriter(file, true)); // true para agregar al archivo existente
+
+            String userData = nombre + "," + usuario + "," + email + "," + contrasenia;
+            writer.write(userData);
+            writer.newLine();
+            writer.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private boolean usuarioExiste(String usuario, String email) {
+        File file = new File(getFilesDir(), "usuarios.csv");
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader(file));
+            String line;
+            while ((line = reader.readLine()) != null) {
+                String[] userData = line.split(",");
+                if (userData.length >= 3 && (usuario.equals(userData[1]) || email.equals(userData[2]))) {
+                    return true;
+                }
+            }
+            reader.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return false;
     }
 }
