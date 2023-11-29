b8f1445a3a99dac7d2812ba4596369f5ab79267d
RodriCz <rodrigocruz_96@hotmail.com>
2023-11-29 17:40:47 -0300

    Dowload imagen se continua

--- app/src/main/java/com/example/taskapp/ImageDownloader.java
+++ app/src/main/java/com/example/taskapp/ImageDownloader.java
@@ -1,16 +1,27 @@
 package com.example.taskapp;
 
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.AsyncTask;
 import android.util.Log;
+
+import java.io.InputStream;
+import java.net.URL;
 
 public class ImageDownloader extends AsyncTask<String, Integer, Bitmap> {
 
     private Bitmap imagenDescargada = null;
     @Override
     protected Bitmap doInBackground(String... strings) {
-        String URL = strings[0];
-        Log.i("Testing", URL);
+        String Url_consulta = strings[0];
+        try {
+            URL consulta = new URL(Url_consulta);
+            InputStream contenido = (InputStream) consulta.getContent();
+            this.imagenDescargada = BitmapFactory.decodeStream(contenido);
+        }catch (Exception e)
+        {
+            e.printStackTrace();
+        }
         return imagenDescargada;
     }
 
