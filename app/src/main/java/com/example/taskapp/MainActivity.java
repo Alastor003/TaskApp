3ed7206ecc165ec4737ad412542e1218cdcb00b5
RodriCz <rodrigocruz_96@hotmail.com>
2023-12-02 22:21:35 -0300

    Conexión a Internet Mensaje invisible de alerta

--- app/src/main/java/com/example/taskapp/MainActivity.java
+++ app/src/main/java/com/example/taskapp/MainActivity.java
@@ -5,12 +5,16 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.media.Image;
+import android.net.ConnectivityManager;
+import android.net.Network;
+import android.net.NetworkInfo;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
@@ -32,6 +36,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        checkConnection();
 
         mAuth = FirebaseAuth.getInstance();
         db = FirebaseFirestore.getInstance();
@@ -125,5 +130,23 @@ public class MainActivity extends AppCompatActivity {
         }
         ImageDownloader descarga = new ImageDownloader(imageView);
         descarga.execute("https://scontent.fepa2-1.fna.fbcdn.net/v/t39.30808-6/405251742_7673753482641159_1720167608337291579_n.jpg?_nc_cat=109&ccb=1-7&_nc_sid=3635dc&_nc_ohc=lAk3tujHHJsAX-buVqX&_nc_ht=scontent.fepa2-1.fna&cb_e2o_trans=t&oh=00_AfAVcX33uj07QQCdZ86SH9nAaQd8_Wcn6wFmCTFmvvaN6A&oe=656BB993");
+    }
+
+    //Conexión y verificación de internet.
+    public  void checkConnection ()
+    {
+        LinearLayout noInternetConnection = findViewById(R.id.noConnectionLayout);
+
+        ConnectivityManager connectivityManager = (ConnectivityManager) getSystemService(CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = connectivityManager.getActiveNetworkInfo();
+        if (networkInfo != null && networkInfo.isConnected())
+        {
+            noInternetConnection.setVisibility(View.INVISIBLE);
+
+        }else
+        {
+            noInternetConnection.setVisibility(View.VISIBLE);
+        }
+
     }
 }
